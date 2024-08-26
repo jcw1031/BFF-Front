11c0d2b1e935d29271701aa70b799d476b5983d7
지찬우 <jcw001031@gmail.com>
2024-08-26 13:17:23 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,18 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1/restaurant-summary': {
-        target: 'http://localhost:8080/api/v1/restaurant-summary',
+        target: 'http://3.34.104.102:8080/api/v1/restaurant-summary',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/v1\/restaurant-summary/, '')
       },
       '/api/v1/restaurants': {
-        target: 'http://localhost:7002/api/v1/restaurants',
+        target: 'http://3.38.180.124:7002/api/v1/restaurants',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/v1\/restaurants/,
             '')
       },
       '/api/v1/menus': {
-        target: 'http://localhost:7002/api/v1/menus',
+        target: 'http://3.38.180.124:7002/api/v1/menus',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/v1\/menus/, '')
       }
